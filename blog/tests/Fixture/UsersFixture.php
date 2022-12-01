7b9193500f17e7d8bf8e934b564921018254314b
sunamoto-mikiya <miki38gtr@gmail.com>
2022-12-01 09:28:17 +0900

    bakeで作成されたuser関連のテスト

---
+++ blog/tests/Fixture/UsersFixture.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'email' => 'Lorem ipsum dolor sit amet',
+                'password' => 'Lorem ipsum dolor sit amet',
+                'role' => 'Lorem ipsum dolor ',
+                'created' => '2022-11-30 23:41:55',
+                'modified' => '2022-11-30 23:41:55',
+            ],
+        ];
+        parent::init();
+    }
+}
